4c6b01c018815a5ee0a85a83000514a6fec1eef3
Pezhman1994 <pezhmanheidaryrostamy1994@gmail.com>
2023-11-14 19:28:42 +0330

    Add C# code examples for the use of 'ref' keyword

---
+++ ref_keyword/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void ChangeValue(ref int number)
+    {
+        number = 10; // تغییر مقدار پارامتری که به عنوان مرجع داده شده است
+    }
+
+    static void Main(string[] args)
+    {
+        int value = 5;
+        Console.WriteLine($"Before method call: {value}");
+
+        // فراخوانی متد با استفاده از ref
+        ChangeValue(ref value);
+
+        Console.WriteLine($"After method call: {value}"); // خروجی: 10
+    }
+}
