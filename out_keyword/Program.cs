6b042068fdfa2dc766def5d3e652b6c7cab4f9b4
Pezhman1994 <pezhmanheidaryrostamy1994@gmail.com>
2023-11-14 18:02:40 +0330

    Add C# code examples for the use of 'out' keyword

---
+++ out_keyword/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    // Example using out
+    static void Divide(int dividend, int divisor, out int quotient, out int remainder)
+    {
+        // Checking division by zero
+        if (divisor != 0)
+        {
+            quotient = dividend / divisor;
+            remainder = dividend % divisor;
+        }
+        else
+        {
+            quotient = 0;
+            remainder = 0;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int dividend = 10;
+        int divisor = 3;
+        int resultQuotient;
+        int resultRemainder;
+
+        // Using out
+        Divide(dividend, divisor, out resultQuotient, out resultRemainder);
+
+        Console.WriteLine($"Quotient: {resultQuotient}, Remainder: {resultRemainder}");
+    }
+}
